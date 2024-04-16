28d0cc3b968c5a2b460fa8aa3208a3dcb02da1fd
timothy <timothy.joblin@gmail.com>
2024-04-16 10:39:57 +1000

    feat: Switching scenes I made a method in HelloApplication to assist with switching the root of scenes/ go to a new page.
    to use it in another class:
    HelloApplication.changeScene("new-routine-view.fxml");

--- src/main/java/org/trainer/interval_trainer/HelloApplication.java
+++ src/main/java/org/trainer/interval_trainer/HelloApplication.java
@@ -2,14 +2,19 @@ package org.trainer.interval_trainer;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
+    private static Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
+        primaryStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
@@ -17,7 +22,8 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
+    public static void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource(fxml)));
+        primaryStage.getScene().setRoot(pane);
     }
 }
--- src/main/java/org/trainer/interval_trainer/HelloController.java
+++ src/main/java/org/trainer/interval_trainer/HelloController.java
@@ -1,7 +1,12 @@
 package org.trainer.interval_trainer;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -10,5 +15,10 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+
+    public void onNewRoutineButton() throws IOException {
+        HelloApplication.changeScene("new-routine-view.fxml");
     }
 }
