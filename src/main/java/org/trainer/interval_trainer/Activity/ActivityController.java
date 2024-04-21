1ac142849d93b2a6ffe7613661c7dd80eeae38e2
Mini412 <145810429+Mini412@users.noreply.github.com>
2024-04-21 13:06:19 +1000

    Added Laura Branch and started work on Activity Page

---
+++ src/main/java/org/trainer/interval_trainer/Activity/ActivityController.java
@@ -0,0 +1,18 @@
+package org.trainer.interval_trainer.Activity;
+
+import javafx.fxml.FXML;
+
+
+public class ActivityController {
+
+    @FXML
+    protected void onArrowButtonCLick() {
+    //Return to Routines page
+    }
+
+    @FXML
+    protected void onPauseButtonCLick() {
+    //Pause Timer
+    }
+
+}
