f2bdb39d06c67b820810338fcd20cfaf4880eb80
finlopes97 <Kyle.Finlayson1997@gmail.com>
2024-04-21 21:53:20 +1000

    !feat(test): add tests for user validation
    All tests are written, validation class is also complete and ready to use. Still encountering build errors and cannot utilise JUnit tests.

--- src/main/java/org/trainer/interval_trainer/test/UserRegistrationValidatorTest.java
+++ src/main/java/org/trainer/interval_trainer/test/UserRegistrationValidatorTest.java
@@ -9,4 +9,58 @@ public class UserRegistrationValidatorTest {
         UserRegistrationValidator userRegistrationValidator = new UserRegistrationValidator();
         assertTrue(userRegistrationValidator.isValidEmail("user@email.com"));
     }
+
+    @Test
+    public void isValidUsername() {
+        UserRegistrationValidator userRegistrationValidator = new UserRegistrationValidator();
+        assertTrue(userRegistrationValidator.isvalidUsername("username123"));
+    }
+
+    @Test
+    public void isInvalidUsername() {
+        UserRegistrationValidator userRegistrationValidator = new UserRegistrationValidator();
+        assertFalse(userRegistrationValidator.isvalidUsername("<!special!>"));
+    }
+
+    @Test
+    public void isUsernameTooLong() {
+        UserRegistrationValidator userRegistrationValidator = new UserRegistrationValidator();
+        assertFalse(userRegistrationValidator.isvalidUsername("hack the planet!!!"));
+    }
+
+    @Test
+    public void isUsernameTooShort() {
+        UserRegistrationValidator userRegistrationValidator = new UserRegistrationValidator();
+        assertFalse(userRegistrationValidator.isvalidUsername("UwU"));
+    }
+
+    @Test
+    public void isValidPassword() {
+        UserRegistrationValidator userRegistrationValidator = new UserRegistrationValidator();
+        assertTrue(userRegistrationValidator.isValidPassword("password123"));
+    }
+
+    @Test
+    public void isPasswordAbleToUseSpecialCharacters() {
+        UserRegistrationValidator userRegistrationValidator = new UserRegistrationValidator();
+        assertTrue(userRegistrationValidator.isValidPassword("password!@#"));
+    }
+
+    @Test
+    public void isInvalidPassword() {
+        UserRegistrationValidator userRegistrationValidator = new UserRegistrationValidator();
+        assertFalse(userRegistrationValidator.isValidPassword("no spaces in password"));
+    }
+
+    @Test
+    public void isPasswordTooShort() {
+        UserRegistrationValidator userRegistrationValidator = new UserRegistrationValidator();
+        assertFalse(userRegistrationValidator.isValidPassword("guest"));
+    }
+
+    @Test
+    public void isPasswordTooLong() {
+        UserRegistrationValidator userRegistrationValidator = new UserRegistrationValidator();
+        assertFalse(userRegistrationValidator.isValidPassword("residentevildeadaimdoesntgetenoughcreditforbeingafunlightgungameonthesonyplaystation2"));
+    }
 }
--- src/main/java/org/trainer/interval_trainer/validation/UserRegistrationValidator.java
+++ src/main/java/org/trainer/interval_trainer/validation/UserRegistrationValidator.java
@@ -3,6 +3,17 @@ package org.trainer.interval_trainer.validation;
 public class UserRegistrationValidator {
 
     public boolean isValidEmail(String address) {
+        // Email addresses must contain an '@' symbol
         return address != null && address.contains("@");
+    }
+
+    public boolean isvalidUsername(String username) {
+        // Username must be made up of alphanumeric characters and be at least four characters long
+        return username.matches("^[a-zA-Z0-9_-]{3,15}$");
+    }
+
+    public boolean isValidPassword(String password) {
+        // Valid passwords must be made up of alphanumeric characters and be at least six characters long
+        return password.matches("^[a-zA-Z0-9_./~!@#$%^*()&+-]{6,30}$");
     }
 }
