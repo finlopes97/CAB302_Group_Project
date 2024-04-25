285bade6404d39822ebfceb3663fa9ec676cc112
finlopes97 <Kyle.Finlayson1997@gmail.com>
2024-04-25 21:02:48 +1000

    feat(db): add temp model for shared routine

---
+++ src/main/java/org/trainer/interval_trainer/models/TestRoutine.java
@@ -0,0 +1,12 @@
+package org.trainer.interval_trainer.models;
+
+public class TestRoutine {
+    private int id;
+    private String name;
+    private String created_by;
+    private String created_on;
+    private String description;
+    private int total_time;
+    private String[] groups;
+//    private
+}
