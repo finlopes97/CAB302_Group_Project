46081e67e12d8ee55c7223fa5bc003a63546bf9d
Mini412 <145810429+Mini412@users.noreply.github.com>
2024-04-29 10:06:53 +1000

    Added timer

--- src/main/java/org/trainer/interval_trainer/Activity/ActivityController.java
+++ src/main/java/org/trainer/interval_trainer/Activity/ActivityController.java
@@ -1,9 +1,14 @@
 package org.trainer.interval_trainer.Activity;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.util.Duration;
 import org.trainer.interval_trainer.HelloApplication;
-
+import org.trainer.interval_trainer.NewRoutine.Interval;
 import java.io.IOException;
+import javafx.scene.control.Label;
+
 
 
 public class ActivityController {
@@ -15,11 +20,40 @@ public class ActivityController {
 
     @FXML
     protected void onPauseButtonClick() {
-    //Pause Timer
+    if (timeline != null) {
+        timeline.stop();
+    }
+    }
+    private Timeline timeline;
+    private static Integer seconds = 30;
+    @FXML
+    private Label timerLabel;
+    @FXML
+    protected void timer() {
+
+        timerLabel.setText(seconds.toString());
+        timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.getKeyFrames().add(
+                new KeyFrame(Duration.seconds(1),
+                        event -> {
+                            seconds--;
+                            timerLabel.setText(seconds.toString());
+                            if (seconds <= 0) {
+                                timeline.stop();
+                            }
+                        })
+        );
+        timeline.playFromStart();
+
     }
     @FXML
     protected void onStartButtonClick() throws IOException {
         HelloApplication.changeScene("activity-start-view.fxml");
+        timer();
     }
+
+
+
 
 }
