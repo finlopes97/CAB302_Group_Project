5b47d4554c4f8bf359f0cf1e833a28ab472fd7fd
finlopes97 <kyle.finlayson1997@gmail.com>
2024-04-29 11:18:09 +1000

    fix(modules): fixed tests oml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,30 @@
+//module org.trainer.interval_trainer {
+//    requires javafx.controls;
+//    requires javafx.fxml;
+//    requires java.sql;
+//
+//
+//    opens org.trainer.interval_trainer to javafx.fxml;
+//    exports org.trainer.interval_trainer;
+//    exports org.trainer.interval_trainer.NewRoutine;
+//    exports org.trainer.interval_trainer.Activity;
+//    opens org.trainer.interval_trainer.NewRoutine to javafx.fxml;
+//    opens org.trainer.interval_trainer.Activity to javafx.fxml;
+//}
+
 module org.trainer.interval_trainer {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
+    requires junit;
 
     opens org.trainer.interval_trainer to javafx.fxml;
     exports org.trainer.interval_trainer;
     exports org.trainer.interval_trainer.NewRoutine;
     exports org.trainer.interval_trainer.Activity;
+    exports org.trainer.interval_trainer.test to junit;
+    exports org.trainer.interval_trainer.controller;
     opens org.trainer.interval_trainer.NewRoutine to javafx.fxml;
+    opens org.trainer.interval_trainer.controller to javafx.fxml;
     opens org.trainer.interval_trainer.Activity to javafx.fxml;
 }
