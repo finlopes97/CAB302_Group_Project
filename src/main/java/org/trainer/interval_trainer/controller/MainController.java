355a08a12e6ccbad1374345ea0367c68b4d64f5f
finlopes97 <Kyle.Finlayson1997@gmail.com>
2024-05-05 19:59:07 +1000

    fix(fxml): implement dynamic page title
    Left in an error where an fx:id was not being populated in the MainController, fixed it.

--- src/main/java/org/trainer/interval_trainer/controller/MainController.java
+++ src/main/java/org/trainer/interval_trainer/controller/MainController.java
@@ -3,21 +3,36 @@ package org.trainer.interval_trainer.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainController {
     @FXML
     private BorderPane mainLayout;
-
     @FXML
-    private VBox contentArea; // Make sure this is correctly fx:id linked to your FXML
+    private VBox contentArea;
+    @FXML
+    private Label viewTitle;
+
+    private Map<String, String> pageTitleMap = new HashMap<>();
+
+    public MainController() {
+        pageTitleMap.put("/org/trainer/interval_trainer/home-view.fxml", "Home");
+        pageTitleMap.put("/org/trainer/interval_trainer/create-routine-view.fxml", "My Routines");
+        pageTitleMap.put("/org/trainer/interval_trainer/search-view.fxml", "Find Routines");
+        pageTitleMap.put("/org/trainer/interval_trainer/profile-view.fxml", "Profile");
+        pageTitleMap.put("/org/trainer/interval_trainer/settings-view.fxml", "Settings");
+    }
 
     public void switchContent(String fxmlFile) {
         try {
             Node node = FXMLLoader.load(getClass().getResource(fxmlFile));
             contentArea.getChildren().setAll(node);
+            viewTitle.setText(pageTitleMap.getOrDefault(fxmlFile, "Login"));
         } catch (IOException e) {
             e.printStackTrace();
             // Handle exceptions, possibly show an error message
@@ -31,7 +46,7 @@ public class MainController {
 
     @FXML
     private void onMyRoutinesButtonClick() {
-        switchContent("/org/trainer/interval_trainer/create-routines-view.fxml");
+        switchContent("/org/trainer/interval_trainer/create-routine-view.fxml");
     }
 
     @FXML
