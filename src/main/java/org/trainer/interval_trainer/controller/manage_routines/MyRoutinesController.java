297ba42913beea7747f78d0bfb7f8d0bccbc464e
finlopes97 <kyle.finlayson1997@gmail.com>
2024-05-13 10:06:34 +1000

    chore: clean up manage_routines/MyRoutinesController.java

--- src/main/java/org/trainer/interval_trainer/controller/manage_routines/MyRoutinesController.java
+++ src/main/java/org/trainer/interval_trainer/controller/manage_routines/MyRoutinesController.java
@@ -1,22 +1,12 @@
 package org.trainer.interval_trainer.controller.manage_routines;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import org.trainer.interval_trainer.HelloApplication;
 import org.trainer.interval_trainer.Model.Routine;
 import org.trainer.interval_trainer.Model.SqliteRoutinesDAO;
-import org.trainer.interval_trainer.controller.new_routine.RoutineController;
 
 import java.io.IOException;
-import java.sql.*;
-import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -24,9 +14,6 @@ import java.util.List;
  * This controller handles the display and interaction with the list of existing training routines.
  */
 public class MyRoutinesController {
-
-    private static final String DB_URL = "jdbc:sqlite:./src/main/resources/Database.db";
-    @FXML private Button createRoutineButton;
     @FXML VBox routinesVBox;
 
     /**
